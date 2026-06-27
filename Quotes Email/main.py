4709f51c74be6035b226744d9f24d22f0c744ae5
Rijwin Prince Rajesh <rijwinprince@gmail.com>
2026-06-27 15:15:58 +0530

    Update main.py

--- Quotes Email/main.py
+++ Quotes Email/main.py
@@ -1,1 +1,29 @@
+import smtplib
+import datetime as dt
+import random
+MY_EMAIL = "rijwinprince2007@gmail.com"
+MY_PASSWORD = "zkir zdyj fkuv lvio"
+
+
+now=dt.datetime.now()
+weekday=now.weekday()
+if weekday==1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login("MY_EMAIL","MY_PASSWORD")
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"subject:Monday Motivation\n\n{quote}"
+        )
+
+
+
+
 # Your code goes here
